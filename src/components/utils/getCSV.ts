2a9799f4020a848e6eafbcfac82764ffa094ce4e
Steadystudy <sgmin206@naver.com>
2023-01-27 18:33:14 +0900

    Feat: CSV 파일 받은 후 json으로 변환하는 함수

---
+++ src/components/utils/getCSV.ts
@@ -0,0 +1,35 @@
+import Papa, { ParseResult } from 'papaparse';
+
+function Validate(file: string) {
+  const regex = /\.csv$/;
+  if (!regex.test(file)) throw new Error('csv 파일이 아닙니다.');
+}
+
+async function toJson(file: string) {
+  return new Promise((resolve, reject) => {
+    Papa.parse(file, {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      delimiter: ',',
+      complete: (results) => {
+        resolve(results.data);
+      },
+      error(err) {
+        reject(err);
+      },
+    });
+  });
+}
+
+const getCSV = async (file: string) => {
+  try {
+    Validate(file);
+    const data = await toJson(file);
+    return data;
+  } catch (err) {
+    console.error('Could not parse json', err);
+  }
+};
+
+export default getCSV;
