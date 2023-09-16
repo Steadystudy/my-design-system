c89052af71bdf97629a25e2a4118c0826d69df8b
Steadystudy <sgmin206@naver.com>
2023-09-17 01:03:39 +0900

    Chore: GlobalStyle 및 theme 설정

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    };
+
+    ul, li, ol {
+      list-style: none;
+    }
+
+    
+`;
+
+export default GlobalStyles;
---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+const Theme = {
+  colors: {
+    black: '#000000',
+    white: '#FFFFFF',
+    blue100: '#eaf9ff',
+    blue200: '#A0C4FF',
+    blue300: '#bbebff',
+    blue400: '#4285F4',
+    blue500: '#00b2ff',
+    blue600: '#009ee2',
+    blue700: '#006f9f',
+    blue800: '#004765',
+    gray100: '#F2F2F2',
+    gray200: '#E0E0E0',
+    gray300: '#BDBDBD',
+    gray400: '#828282',
+    gray500: '#4F4F4F',
+    gray600: '#333333',
+  },
+  fontSize: {
+    large: '32px',
+    medium: '28px',
+    small: '0.875rem',
+  },
+};
+
+export default Theme;
