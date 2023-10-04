5568b092ad3de054a8b477652729096e2599647d
Steadystudy <sgmin206@naver.com>
2023-10-04 16:28:24 +0900

    Feat: Carousel Drag 기능 추가

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useRef, useState } from 'react';
 import {
   Container,
   Dot,
@@ -17,10 +17,17 @@ interface Image {
 }
 
 export interface CarouselProps {
+  /** Carousel 너비를 설정합니다.*/
   width: number;
+  /** Carousel 높이를 설정합니다.*/
   height: number;
+  /** Carousel에 넣을 이미지들을 설정합니다.*/
   images: Image[];
-  effect?: 'fade' | 'slide';
+  /** Carousel의 type을 설정합니다. */
+  type?: 'fade' | 'slide';
+  /** 드래그 여부 설정합니다. */
+  isDraggable?: boolean;
+  /** Carousel 위에 마우스 올렸을 때 화살표 방향 보여줄 지 설정합니다. */
   showOnHover?: boolean;
 }
 
@@ -28,11 +35,13 @@ const Carousel = ({
   width,
   height,
   images,
-  effect = 'fade',
+  type = 'fade',
+  isDraggable = false,
   showOnHover = false,
 }: CarouselProps) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [translateX, setTranslateX] = useState(0);
+  const slideRef = useRef<HTMLDivElement>(null);
 
   const handleSlider = (index: number) => (event: MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
@@ -41,11 +50,55 @@ const Carousel = ({
     setTranslateX(0);
   };
 
+  const limitToRange = (value: number, minValue: number, maxValue: number) => {
+    if (value < minValue) return minValue;
+
+    if (value > maxValue) return maxValue;
+
+    return value;
+  };
+
+  const handleDragChange = (offsetX: number) => {
+    setTranslateX(limitToRange(offsetX, -width, width));
+  };
+
+  const handleDragEnd = (offsetX: number) => {
+    const maxPosition = images.length - 1;
+
+    if (offsetX < -50) setActiveIndex(limitToRange(activeIndex + 1, 0, maxPosition));
+    if (offsetX > 50) setActiveIndex(limitToRange(activeIndex - 1, 0, maxPosition));
+
+    setTranslateX(0);
+  };
+
+  const handleSliderMouseDown = (clickEvent: MouseEvent<HTMLDivElement>) => {
+    const handleMouseMove = (moveEvent: globalThis.MouseEvent) => {
+      const offsetX = moveEvent.pageX - clickEvent.pageX;
+      handleDragChange(offsetX);
+    };
+
+    const handleMouseUp = (moveEvent: globalThis.MouseEvent) => {
+      const offsetX = moveEvent.pageX - clickEvent.pageX;
+      handleDragEnd(offsetX);
+
+      slideRef.current?.removeEventListener('mousemove', handleMouseMove);
+    };
+
+    slideRef.current?.addEventListener('mousemove', handleMouseMove);
+    slideRef.current?.addEventListener('mouseup', handleMouseUp, { once: true });
+  };
+
   return (
     <Container $width={width} $height={height} className="image_carousel_container">
-      {effect === 'slide' && (
+      {type === 'slide' && (
         <>
-          <SliderContainer $activeIndex={activeIndex} $width={width} $translateX={translateX}>
+          <SliderContainer
+            ref={slideRef}
+            onMouseDown={isDraggable ? handleSliderMouseDown : undefined}
+            $activeIndex={activeIndex}
+            $width={width}
+            $translateX={translateX}
+          >
             {images.map(({ imageUrl, alt }, index) => (
               <ImageWrapper key={index} $width={width} $height={height}>
                 <img
@@ -70,9 +123,12 @@ const Carousel = ({
         </>
       )}
 
-      {effect === 'fade' && (
+      {type === 'fade' && (
         <>
-          <FadeSliderContainer $activeIndex={activeIndex}>
+          <FadeSliderContainer
+            onMouseDown={isDraggable ? handleSliderMouseDown : undefined}
+            $activeIndex={activeIndex}
+          >
             {images.map(({ imageUrl, alt }, index) => (
               <FadeImageWrapper key={index} $width={width} $height={height}>
                 <img
